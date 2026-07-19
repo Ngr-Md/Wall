1bdc67994645983202dda39cde4b30bfa46b2221
Negar <negarmodaghegh@gmail.com>
2026-07-19 20:45:43 +0330

    Create Spring Boot project with health endpoint

---
+++ backend/src/main/java/ir/aut/ap/wall/WallApplication.java
@@ -0,0 +1,12 @@
+package ir.aut.ap.wall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WallApplication.class, args);
+    }
+}
