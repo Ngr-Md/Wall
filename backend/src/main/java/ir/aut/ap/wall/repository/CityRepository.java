9069a4673e849f9b46c30a76299236fe03fec82f
Negar <negarmodaghegh@gmail.com>
2026-07-20 02:10:09 +0330

    Add City and Category repositories

---
+++ backend/src/main/java/ir/aut/ap/wall/repository/CityRepository.java
@@ -0,0 +1,10 @@
+package ir.aut.ap.wall.repository;
+
+import ir.aut.ap.wall.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+    Optional<City> findByName(String name);
+}
