0e61dc340d4da37d8d8f23ab64b77155e2e527b4
mimir <ilia.h.b.1385@gmail.com>
2026-07-21 00:51:39 +0330

    small changes to folder names

--- frontend/src/main/java/ir/aut/ap/marketplace/client/config/ApiConfig.java
+++
@@ -1,8 +0,0 @@
-package ir.aut.ap.marketplace.client.config;
-
-public final class ApiConfig {
-
-    public static final String BASE_URL = "http://localhost:8080";
-    private ApiConfig() {
-    }
-}
