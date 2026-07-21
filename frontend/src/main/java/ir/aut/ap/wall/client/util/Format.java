e33f73e62c8b2671288b3dfd8752ee9f0b8b0d3b
mimir <ilia.h.b.1385@gmail.com>
2026-07-21 16:47:28 +0330

    changed the look of message time display a little

--- frontend/src/main/java/ir/aut/ap/wall/client/util/Format.java
+++ frontend/src/main/java/ir/aut/ap/wall/client/util/Format.java
@@ -12,13 +12,14 @@ public final class Format {
         return NumberFormat.getInstance(Locale.US).format(price) + " چندرغاز";
     }
 
-    private static final java.time.format.DateTimeFormatter MESSAGE_TIME =
-            java.time.format.DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm")
-                    .withZone(java.time.ZoneId.systemDefault());
-
     public static String messageTime(String isoInstant) {
         try {
-            return MESSAGE_TIME.format(java.time.Instant.parse(isoInstant));
+            java.time.ZonedDateTime dateTime = java.time.Instant.parse(isoInstant)
+                    .atZone(java.time.ZoneId.systemDefault());
+            String pattern = dateTime.toLocalDate().equals(java.time.LocalDate.now())
+                    ? "HH:mm"
+                    : "yyyy/MM/dd HH:mm";
+            return dateTime.format(java.time.format.DateTimeFormatter.ofPattern(pattern));
         } catch (Exception e) {
             return "";
         }
